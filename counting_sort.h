56e1b6b07ca0e76cef19dc691b0df7a2be082328
alonso1311 <alonso.barrios@utec.edu.pe>
2020-04-11 01:07:39 -0500

    update

--- counting_sort.h
+++ counting_sort.h
@@ -4,16 +4,23 @@
 #include"header.h"
 
 void count_sort(int array[]){
-    int output[size_array], count[last+1];    
+    //-Step 1
+    int count[last+1];
 
     memset(count, 0, sizeof(count));
         
+    //-Step 2
     for(int i = 0; i < size_array; ++i) 
         count[array[i]]++;
 
+    //-Step 3
     for(int i = 1; i <= last+1; ++i)
         count[i] += count[i-1];
 
+    //-Step 4
+    int output[size_array];
+
+    //-Step 5
     for(int i = 0; i < size_array; ++i){
         output[count[array[i]]-1] = array[i];
         count[array[i]]--;
